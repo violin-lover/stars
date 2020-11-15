1e584c6046d55f06e44dc5b0eb3255b0b4c9de79
SnowKitty <jcaibmi2@gmail.com>
2020-11-15 04:13:30 +0000

    Can now detect if URL isn't valid

--- public/script.js
+++ public/script.js
@@ -1,19 +1,26 @@
 function shorten() {
-  debugger;
   let url = document.getElementById("url").value;
-  getLongURl(url);
+
+    try {
+      new URL(url)
+      getLongURL(url)
+    }
+
+    catch(error) {
+	    document.getElementById("microURL").innerText = "Invalid URL detected. Please input a different URL or check that your URL was copied in correctly."
+    }
 }
 
-function getLongURl(url) {
-  let result = document.getElementById("microURL");
-  let encodedUrl = btoa(url);
-  encodedUrl = encodeURIComponent(encodedUrl);
-  let uRl = "/tokenizer/" + encodedUrl;
-  
-  fetch(uRl)
-    .then((response) => response.json())
+function getLongURL(url){
+let result = document.getElementById("microURL");
+let encodedUrl = btoa(url);
+encodedUrl = encodeURIComponent(encodedUrl);
+let uRl = "/tokenizer/" + encodedUrl;
 
-    .then((res) => {
-      result.innerHTML = res.shortenUrl;
-  });
+fetch(uRl)
+.then(response => response.json())
+
+.then(res => {
+result.innerHTML = res.shortenUrl;
+})
 }
